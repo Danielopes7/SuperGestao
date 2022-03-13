7e8214b5e2a7d092d8217eccf15d9dd12ee82242
Daniel Lopes <daniel.lopes.amorim@hotmail.com>
2022-03-13 17:53:20 -0300

    add seeders

---
+++ database/seeders/FornecedorSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Fornecedor;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+class FornecedorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Instanciando 
+        $fornecedor = new Fornecedor();
+        $fornecedor->nome = 'Fornecedor 100';
+        $fornecedor->site = 'fornecedor100.com.br';
+        $fornecedor->uf = 'CE';
+        $fornecedor->email = 'contato@fornecedor100.com.br';
+        $fornecedor->save();
+
+        // Metodo create
+        Fornecedor::create([
+            'nome' => 'Fornecedor 200',
+            'site' => 'Fornecedor200.com.br',
+            'uf' => 'SP',
+            'email' => 'contato@fornecedor2000.com.br'
+        ]);
+
+        //insert
+        DB::table('fornecedores')->insert([
+            'nome' => 'Fornecedor 300',
+            'site' => 'Fornecedor300.com.br',
+            'uf' => 'MA',
+            'email' => 'contato@fornecedor300.com.br'
+        ]);
+    }
+}
