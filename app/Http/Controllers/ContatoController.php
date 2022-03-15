29b82947674adaf1dc8b8b70a5d0d1f25c6a57dc
Daniel Lopes <daniel.lopes.amorim@hotmail.com>
2022-03-14 22:30:59 -0300

    add factories and validates

--- app/Http/Controllers/ContatoController.php
+++ app/Http/Controllers/ContatoController.php
@@ -1,13 +1,35 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\SiteContato;
 use Illuminate\Http\Request;
 
 class ContatoController extends Controller
 {
-    public function contato(){
-        var_dump($_POST);
-        return view('site.contato');
+    public function contato(Request $request){
+        // $contato = new SiteContato();
+        // $contato->nome = $request->input('nome');
+        // $contato->telefone = $request->input('telefone');
+        // $contato->email = $request->input('email'); 
+        // $contato->motivo_contato = $request->input('motivo_contato');
+        // $contato->mensagem = $request->input('mensagem');
+
+        // //print_r($contato->getAttributes());
+        // $contato->save();
+        //$contato = new SiteContato();
+        //$contato->fill($request->all());
+        //$contato->save();
+
+        return view('site.contato', ['titulo' => 'Contato (teste)']);
+    }
+    public function salvar(Request $request){
+        //validação do request
+        $request->validate([
+            'nome' => 'required|min::3|max::40',
+            'telefone' => 'required'
+
+        ]);
+        //
+        //SiteContato::create($request->all());
     }
 }
