8b48a9278fe12a613e598816a775e1383ea859eb
Daniel Lopes <daniel.lopes.amorim@hotmail.com>
2022-03-20 17:23:05 -0300

    create LoginController

--- routes/web.php
+++ routes/web.php
@@ -22,12 +22,14 @@ Route::get('/sobre-nos','SobreNosController@sobrenos')->name('site.sobrenos');
 Route::get('/contato','ContatoController@contato')->name('site.contato');
 Route::post('/contato','ContatoController@salvar')->name('site.contato');
 
-Route::get('/login',function(){return 'login';})->name('site.login');
+Route::get('/login','LoginController@index')->name('site.login');
+Route::post('/login','LoginController@autenticar')->name('site.login');
 
 Route::middleware('autenticacao')->prefix('/app')->group(function(){
-    Route::get('/clientes',function(){return 'Clientes';})->name('app.clientes');
-    Route::get('/fornecedores','FornecedorController@index')->name('app.fornecedores');
+
     Route::get('/produtos',function(){return 'produtos';})->name('app.produtos'); 
+    Route::get('/fornecedores',function(){return 'fornecedores';})->name('app.fornecedores'); 
+    Route::get('/clientes',function(){return 'clientes';})->name('app.clientes'); 
 });
 
 Route::get('/rota1', function(){
