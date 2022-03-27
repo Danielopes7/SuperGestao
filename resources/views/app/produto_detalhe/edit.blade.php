c8a9fcde822f695856841edc8154c7fdcc326052
Daniel Lopes <daniel.lopes.amorim@hotmail.com>
2022-03-26 21:07:43 -0300

    implementing edit and update controller

--- resources/views/app/produto_detalhe/edit.blade.php
+++ resources/views/app/produto_detalhe/edit.blade.php
@@ -19,7 +19,7 @@
         <div class="informacao-pagina">
             {{ $msg ?? '' }}
             <div style="width: 30%; margin-left: auto; margin-right: auto;">
-                @component('app.produto._detalhe_components.form_create_edit', ['produto_detalhe' => $produto_detalhe, 'unidades' => $unidades])
+                @component('app.produto_detalhe._components.form_create_edit', ['produto_detalhe' => $produto_detalhe, 'unidades' => $unidades])
                 @endcomponent
             </div>
         </div>
